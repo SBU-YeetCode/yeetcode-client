97b509a729af19dbc135adc0b98fb21b8f547e76
Saransh Grover <contact@saranshgrover.com>
2021-04-14 01:17:59 -0400

    Co-authored-by: E-Yoo <E-Yoo@users.noreply.github.com> Co-authored-by: IsaiahGz <IsaiahGz@users.noreply.github.com>

--- pages/game/edit/index.tsx
+++ pages/game/edit/index.tsx
@@ -1,3 +1,13 @@
+import {useUser} from '../../../contexts/UserContext'
+import {useContextGetMeQuery} from '../../../graphql/generated'
 export default function GameEdit() {
-	return <div>Game Edit Page</div>
+	const {data, isLoading, error, refetch} = useContextGetMeQuery()
+	console.log(data)
+	const {user} = useUser()
+	// console.log(user)
+	return (
+		<>
+		<h1>Hello {`${user ? user.name : "lame"}!`}</h1>
+		</>
+	)
 }
